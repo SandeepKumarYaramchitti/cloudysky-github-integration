d7742ab2920571be2708f80974f066efcc14ae06
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-09-15 06:28:28 -0400

    add logic to filter sensitive data

--- app/api/awsservice/route.ts
+++ app/api/awsservice/route.ts
@@ -1,9 +1,45 @@
 import { NextResponse } from 'next/server'
 import { ECS } from 'aws-sdk';
+
+type ExtractedTaskData = {
+  availabilityZone: string;
+  connectivity: string;
+  connectivityAt: string;
+  containers: {
+    name: string;
+    image: string;
+    lastStatus: string;
+    cpu: string;
+    memory: string;
+  }[];
+  cpu: string;
+  memory: string;
+  launchType: string;
+};
+
+const extractTaskData = (data: any): ExtractedTaskData[] => {
+  if (!data.tasks) return [];
+
+  return data.tasks.map((task: any) => ({
+    availabilityZone: task.availabilityZone,
+    connectivity: task.connectivity,
+    connectivityAt: task.connectivityAt,
+    containers: task.containers.map((container: any) => ({
+      name: container.name,
+      image: container.image,
+      lastStatus: container.lastStatus,
+      cpu: container.cpu,
+      memory: container.memory,
+    })),
+    cpu: task.cpu,
+    memory: task.memory,
+    launchType: task.launchType,
+  }));
+};
  
 export async function GET() {
-//   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-//   const data = await res.json()
+
+
   const ecs = new ECS({ region: 'us-east-1' });
   
     const data = await ecs.listTasks({ cluster: 'nextjs-cluster' }).promise();
@@ -12,12 +48,13 @@ export async function GET() {
       throw new Error('No tasks found for the specified cluster.');
     }
     
-    const taskDetails = await ecs.describeTasks({
+    const rawtaskDetails = await ecs.describeTasks({
       cluster: 'nextjs-cluster',
       tasks: data.taskArns
     }).promise();
-
-    return NextResponse.json(taskDetails)
+    
+    const sanitizedData = extractTaskData(rawtaskDetails);
+    return NextResponse.json(sanitizedData)
   
   
 }
